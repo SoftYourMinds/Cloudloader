c55a8c67753e986a3161683644996b9b4fdd3a63
MaximBrShow <maximbarishov06@gmail.com>
2021-12-26 06:42:48 +0200

    upgrade_analogy

--- functions/anagrams.js
+++ functions/anagrams.js
@@ -1,17 +1,22 @@
 function anagrams(word, words) {
-   var ter
+  
 	return words.reduce(function(res, e, index, array){
 	
 		if(words[index].length == word.length){
 			search = e[index].split('');
 	 		for(var i=0; i < word.length; i++){
-				res = res +""+ word.split("").indexOf(search[i]) > -1 ? "true/" : "false/";
+				// res = res +""+ word.split("").indexOf(search[i]) > -1 ? "true/" : "false/";
+				if(word.split("").indexOf(search[i]) > -1){
+					res = res + "true/";
+				} else{
+					res = res +"false/";
+				}
 			}	
 		
 			
 		}
-		return res;
-	 });
+		return res.split("/").indexOf("true") ? true : false;
+	 },"");
 	 
 	 
 	 //result.split("").indexOf("true") ? true : false;
