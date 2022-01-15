a6277937600910ec106b8fc98dedb1250d1312be
MaximBrShow <maximbarishov06@gmail.com>
2022-01-15 14:17:24 +0200

    scroll and mouseInTOp mouseOutDown MouseLeftTOp....

--- functions/codeWars.js
+++ functions/codeWars.js
@@ -1,82 +1,82 @@
 function anagramCounter(wordsArray) { // считает количесво анаграм в строке
-	var counter = 0; 
-	for(var i = 0; i< wordsArray.length; i++){
-	  wordsArray.reduce((r, e)=> {
-			if((e != wordsArray[i])&&(e.length == wordsArray[i].length)){	
-				if(wordsArray[i].split('').sort().reduce((res, el)=>{res = res + el; return res;}, "") == e.split('').sort().reduce((res, el)=>{res = res+el; return res;}, '')){
+	var counter = 0;
+	for (var i = 0; i < wordsArray.length; i++) {
+		wordsArray.reduce((r, e) => {
+			if ((e != wordsArray[i]) && (e.length == wordsArray[i].length)) {
+				if (wordsArray[i].split('').sort().reduce((res, el) => { res = res + el; return res; }, "") == e.split('').sort().reduce((res, el) => { res = res + el; return res; }, '')) {
 					counter++;
 				}
-				
+
 			}
-		
+
 		}, "");
 		console.log(wordsArray);
 	}
-	
-	return counter/2;
+
+	return counter / 2;
 }
 
 //=====================================================================
 
-function pigIt(str){ // редактирует строку таким образом, что первую букву слова переносит в конец слова и после этого добавляет "ay" 
-	var s  = ["!",",",".","'",'"',".","(",")"];
+function pigIt(str) { // редактирует строку таким образом, что первую букву слова переносит в конец слова и после этого добавляет "ay" 
+	var s = ["!", ",", ".", "'", '"', ".", "(", ")"];
 	console.log(str.split(" "));
-	var arr = str.split(' ').reduce((r, e)=>{
-	  if((s.indexOf(e[0]) >-1)&&(s.indexOf(e[e.length])> -1)){  
-		 var k = e[0]+e.slice(1,2, e.length);
-		e = k+e[1]+"ay"+e[e.length];
-		 r = r+" "+e;
-	
-	  } else if((s.indexOf(e[0]) > -1)&&(e.length>1)){
-		  var k = e[0]+e.slice(1,2); 
-		  e = k+e[1] +"ay";
-		  r = r +" "+e;
-		 console.log(4);
-	  } else if(s.indexOf(e[e.length])> -1){
-		var k = e.slice(1,e.length);  
-		e = k +e[0]+"ay"+e.length;
-		 r = r +" "+ e;
-		 console.log(3);
-	  } else if((s.indexOf(e)>-1)&&(e.length == 1)){
-		 r = r +" "+ e; 
-		 console.log(2);
-	  } else {
-		 var k = e.slice(1); 
-		 r = r +" "+ k + e[0] +"ay";
-		 console.log(1);
-	  }	
-	  
-	  return r;
-	},"");
-	return arr.split('').slice(1).reduce((res, el)=>{res=res+el; return res;},"");
+	var arr = str.split(' ').reduce((r, e) => {
+		if ((s.indexOf(e[0]) > -1) && (s.indexOf(e[e.length]) > -1)) {
+			var k = e[0] + e.slice(1, 2, e.length);
+			e = k + e[1] + "ay" + e[e.length];
+			r = r + " " + e;
+
+		} else if ((s.indexOf(e[0]) > -1) && (e.length > 1)) {
+			var k = e[0] + e.slice(1, 2);
+			e = k + e[1] + "ay";
+			r = r + " " + e;
+			console.log(4);
+		} else if (s.indexOf(e[e.length]) > -1) {
+			var k = e.slice(1, e.length);
+			e = k + e[0] + "ay" + e.length;
+			r = r + " " + e;
+			console.log(3);
+		} else if ((s.indexOf(e) > -1) && (e.length == 1)) {
+			r = r + " " + e;
+			console.log(2);
+		} else {
+			var k = e.slice(1);
+			r = r + " " + k + e[0] + "ay";
+			console.log(1);
+		}
+
+		return r;
+	}, "");
+	return arr.split('').slice(1).reduce((res, el) => { res = res + el; return res; }, "");
 }
 
 //=====================================================================
 
 function moveZeros(arr) { // все "0" отправляет в конец массива 
-	return arr.reduce((r, e)=>{ e !== 0 ? r.push(e) : ""; return r }, []).concat(arr.reduce((r, e)=>{ e === 0 ? r.push(e) : ""; return r; },[]));
-} 
+	return arr.reduce((r, e) => { e !== 0 ? r.push(e) : ""; return r }, []).concat(arr.reduce((r, e) => { e === 0 ? r.push(e) : ""; return r; }, []));
+}
 
- //=====================================================================
+//=====================================================================
 
- function highAndLow(numbers){ // ищет минимальный и максимальный элемент в массиве
-	var max = numbers.reduce(function(res, e){
-	  res = res < e ? res = e : res;
-	  return res;
+function highAndLow(numbers) { // ищет минимальный и максимальный элемент в массиве
+	var max = numbers.reduce(function (res, e) {
+		res = res < e ? res = e : res;
+		return res;
 	}, numbers[0]);
-	
-	var min = numbers.reduce(function(res, e){
-	  res = res > e ? res = e : res;
-	  return res;
+
+	var min = numbers.reduce(function (res, e) {
+		res = res > e ? res = e : res;
+		return res;
 	}, numbers[0]);
-	
-	return  min+" "+max;
- }
+
+	return min + " " + max;
+}
 
 //====================================================
 function sumIntervals(intervals) {
 	// var finalArr = [];
-	
+
 	// intervals.forEach(e => {
 	// 	finalArr.length = Math.max(finalArr.length, e[1]);
 	// 	for(var i = e[0]; i<e[1]; ++i){
@@ -85,26 +85,26 @@ function sumIntervals(intervals) {
 	// 	}
 	// 	console.log(finalArr);
 	// });
-	
+
 	// return finalArr.reduce((r, e)=>{ if(e != "empty") r= r+e; return r; },0);
-	 //------------- 
-// 	finalArr = [];
-	
-// 	intervals.forEach(e => {
-// 		for(var i = e[0]; i<e[1]; i++){
-// 			finalArr.push(i);
-// 		}
-// 	});
-//   finalArr = [...new Set(finalArr)];
-// 	return finalArr.length;
- 
+	//------------- 
+	// 	finalArr = [];
+
+	// 	intervals.forEach(e => {
+	// 		for(var i = e[0]; i<e[1]; i++){
+	// 			finalArr.push(i);
+	// 		}
+	// 	});
+	//   finalArr = [...new Set(finalArr)];
+	// 	return finalArr.length;
+
 
 
 	// var finalArr = [];
 	//  for(var i = 0; i<intervals.length; i++){
 	// 	 for(var j = intervals[i][0]; j< intervals[i][1]; j++){
 	// 		finalArr[j] = 1;
-			
+
 	// 	 }
 	//  }
 
@@ -136,10 +136,10 @@ function sumIntervals(intervals) {
 	let sum = 0, max = -Infinity;
 	const sortedIntervals = intervals.sort(([a, b], [c, d]) => a == c ? b - d : a - c);
 	for (const [start, stop] of sortedIntervals) {
-	  if (max < start)
-		 max = start;
-	  if (max < stop)
-		 [max, sum] = [stop, sum + stop - max]
+		if (max < start)
+			max = start;
+		if (max < stop)
+			[max, sum] = [stop, sum + stop - max]
 	}
 	return sum;
 }
@@ -147,130 +147,131 @@ function sumIntervals(intervals) {
 
 //============================================================
 //  3 единицы 1000 очков, 1 пятерка 50 очков и т.д всё рассписано в pints 
-function score( dice ) {  // функция выводит количество набранных очков из массива
-	var points =[[3, 1, 1000], [3,6,600], [3,5,500], [3,4,400],[3,3,300],[3,2,200],[1,1,100],[1,5,50]];
+function score(dice) {  // функция выводит количество набранных очков из массива
+	var points = [[3, 1, 1000], [3, 6, 600], [3, 5, 500], [3, 4, 400], [3, 3, 300], [3, 2, 200], [1, 1, 100], [1, 5, 50]];
 	var score = 0;
 	var k = 0;
 	var arr = new Array();
-	for(var i=0; i< dice.length; i++){
-		if(arr.indexOf(dice[i])==-1){
+	for (var i = 0; i < dice.length; i++) {
+		if (arr.indexOf(dice[i]) == -1) {
 			arr.push(dice[i]);
-			k = dice.reduce((r, e)=>{
-				if(e == dice[i]){
+			k = dice.reduce((r, e) => {
+				if (e == dice[i]) {
 					r++;
 				}
-				return r; 
+				return r;
 			}, 0);
-			points.forEach(([a, b, c])=>{
-				if((a == k)&&(b==dice[i])){
-			  		score = score + parseInt(c);
+			points.forEach(([a, b, c]) => {
+				if ((a == k) && (b == dice[i])) {
+					score = score + parseInt(c);
 				}
 			});
-		}	
-   }
+		}
+	}
 
 	return score;
-}	
+}
 
 //=======================================================
 function multiplicationTable(size) { // выводит таблицу умножения
 	var arr = [];
-	for(var i = 0; i < size; i++){
-		arr[i] = []; 
-	  for(var j = 0; j< size; j++){
-		 arr[i][j] = (i+1)*(j+1);
-	  }
-	}
-	
-	return arr; 
- }
- 
-
- //==============
- function likes(names) { // who like it in codewars
-	return names.reduce((r,e)=>{
-		if(names.length == 0){
-		  r = "no one likes this"; 
-		} else if(names.length == 1){
-		  r = e+" likes this";
-		} else if(names.length == 2){
-		  r = names[0] +" and "+ names[1] + "like this"; 
-		} else if( names.length == 3){
-		  r = names[0] + ", "+ names[1]+ " and "+ names[2] +" like this";
-		} else{
-		//   r =  parseInt(names[0]) +", "+ parseInt(names[1])+ " and " + parseInt(names.length)-2+ " others like this";
-
-		r = names[0] + ", " +names[1] +" and " + (names.length-2) + " others like this";
+	for (var i = 0; i < size; i++) {
+		arr[i] = [];
+		for (var j = 0; j < size; j++) {
+			arr[i][j] = (i + 1) * (j + 1);
 		}
-	  
-	  return r;
-	},"no one likes this");
-  }
+	}
 
-  //===========================================
-  function comp(a1, a2){
+	return arr;
+}
+
+
+//==============
+function likes(names) { // who like it in codewars
+	return names.reduce((r, e) => {
+		if (names.length == 0) {
+			r = "no one likes this";
+		} else if (names.length == 1) {
+			r = e + " likes this";
+		} else if (names.length == 2) {
+			r = names[0] + " and " + names[1] + "like this";
+		} else if (names.length == 3) {
+			r = names[0] + ", " + names[1] + " and " + names[2] + " like this";
+		} else {
+			//   r =  parseInt(names[0]) +", "+ parseInt(names[1])+ " and " + parseInt(names.length)-2+ " others like this";
+
+			r = names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this";
+		}
+
+		return r;
+	}, "no one likes this");
+}
+
+//===========================================
+function comp(a1, a2) {
 	var chek;
-	a1 = a1.reduce((r, e, i, array)=>{
-	  r[i] = parseInt(e)*parseInt(e);
-	  return r;
-	},[]);
+	a1 = a1.reduce((r, e, i, array) => {
+		r[i] = parseInt(e) * parseInt(e);
+		return r;
+	}, []);
 
-	return  a1.length == a2.length && a1.sort().every((v,i)=>v === a2.sort()[i]) ? true : false; 
-  }
+	return a1.length == a2.length && a1.sort().every((v, i) => v === a2.sort()[i]) ? true : false;
+}
 
 //===============================
 
 function firstFactorial(num) {  // 
 	var f;
-	for(var i =1; i <= num; ++i ){
-	  f = parseInt(f) * parseInt(i);
+	for (var i = 1; i <= num; ++i) {
+		f = parseInt(f) * parseInt(i);
 	}
 	return f;
- 
- }
+
+}
 
 
 //================================
 //Wave Sorting
- function waveSort(arr) {
+function waveSort(arr) {
 	var finalArr = [];
 	var k = arr.length;
-	for(var i = 1; i <=k; i++){
-		if(i % 2 != 0){
-			finalArr.push(parseInt(Math.min.apply(null,arr)));          
-			var newArr = arr.slice(arr.indexOf(Math.min.apply(null,arr)));	
+	for (var i = 1; i <= k; i++) {
+		if (i % 2 != 0) {
+			finalArr.push(parseInt(Math.min.apply(null, arr)));
+			var newArr = arr.slice(arr.indexOf(Math.min.apply(null, arr)));
 			console.log(newArr);
 		}
 		else {
-			var max = arr.filter((e)=>parseInt(e)>parseInt(Math.min.apply(null,arr)));
-			
+			var max = arr.filter((e) => parseInt(e) > parseInt(Math.min.apply(null, arr)));
+
 			finalArr.push(parseInt(Math.min.apply(null, max)));
 			newArr
-		   arr.slice(arr.indexOf(parseInt(Math.min.apply(null, max))));
-		} 
+			arr.slice(arr.indexOf(parseInt(Math.min.apply(null, max))));
+		}
 
 	}
-	return finalArr;	 
- }
+	return finalArr;
+}
 
 //========================================
 //Array Addition
 
-function ArrayAddition(arr){
-	return arr.reduce((r, e)=>{
-		var sum = 0; 
-		for(var i = 0; i< arr.length; i++){
-			if(( arr[i] != parseInt(e))&&(arr[i]!= Math.max.apply(null, arr))){
+function arrayAddition(arr) {
+	return arr.reduce((r, e) => {
+		var sum = 0;
+		for (var i = 0; i < arr.length; i++) {
+			if ((arr[i] != parseInt(e)) && (arr[i] != Math.max.apply(null, arr))) {
 				sum = sum + arr[i];
 
 			}
-			if(sum == parseInt(Math.max.apply(null, arr))){
-				r = true;
-			}
+			if (sum == parseInt(Math.max.apply(null, arr))) r = true;
+
 		}
-	
+
 		return r == false ? false : true;
 	}, false);
 }
 
 //======================
+
+
